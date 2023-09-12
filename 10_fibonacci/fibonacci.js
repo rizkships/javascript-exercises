a11c194a80529422c2cb44408a5bc6c057c270d2
rizk <rizkdotai@gmail.com>
2023-09-12 12:40:23 +0300

    Solve exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+if (count < 0) return "OOPS";
+if (count === 0) return 0;
 
-};
+let prevNum = 1
+let nextNum = 0 
+
+for (let i = 2; i <= count; i++ ) {
+   let currentNum = prevNum + nextNum; 
+    nextNum = prevNum;
+    prevNum = currentNum;
+}
+return prevNum
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
