b0f422b16a3654de210f6d9832b06a70138ac4f5
rizk <rizkdotai@gmail.com>
2023-09-28 16:01:12 +0300

    solve exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,28 +1,10 @@
-//const reverseString = function(string) {
-    //if (string = "") return "";
-    // declare a variable that turns component strings into arrays 
-    //let words = string.split(" ")
-    
-    // declare another variable that splits each word in these arrays to characters
-    //let char = words.split('')
-    //let reverseChar = [];
-    // run a loop that pops last character from last array into new array 
-    //for (let i = 0, char.length() > 0, i++)  {
-        //char.pop()
-        //reverseChar += char.pop ;
-    //}
-    
-     //return reverseChar; 
-    // ... then add these characters then adds resulting words from arrays to form sentence with space
 
-
-//};
-function reverseString(str){
-    let r = "";
-    for(let i = str.length-1; i >= 0; i--){
-      r += str[i];
+function reverseString(string){
+    let reversed = "";
+    for(let i = string.length-1; i >= 0; i--){
+      reversed += string[i];
     }
-    return r;
+    return reversed;
   }
 
 // Do not edit below this line
